0f4c7db5e9f1ce42db0253f63c08180f10309611
Whoenix <106924364+wuyutongycy@users.noreply.github.com>
2025-09-26 15:45:14 +0800

    Delete socialZone directory

--- socialZone/vue/config/config.default.js
+++
@@ -1,9 +0,0 @@
-/**
- * 前端项目配置文件
- */
-const projectName = 'Whoenix Zone' // 项目名称
-const ip = '127.0.0.1'
-const port= '9090'
-const serverHost = 'http://'+ip+':'+port // 接口服务器地址 举例:http://127.0.0.1:9090
-
-export { projectName, serverHost }
